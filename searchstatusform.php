8ec4ca34e16f5c35d0afe595a9cf8210e316e3f9
Patrick Baird <patrickjbaird@gmail.com>
2015-05-24 03:59:32 +0000

    Git submit for Sprint 2 work

---
+++ searchstatusform.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="SE.css">
+        <title>SERLER Evidence Repository - Search</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    </head>
+    <body>
+        <header>
+        <h1>Paper Search Form</h1>
+        </header>
+        <section>
+        <form name="searchpaperr" action="searchpaperprocess.php"  method="post">
+        	<label>Search Query:</label><input type="text" name="searchTerm">
+            <input type="submit" value="Submit"><input type="reset" value="Reset">
+        </form>
+        </section> 
+    </body>
+</html>
