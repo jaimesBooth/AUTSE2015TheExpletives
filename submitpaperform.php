d35e465efd920f44b4955165f7faaa0e5e1df8e5
Patrick Baird <patrickjbaird@gmail.com>
2015-05-24 03:51:57 +0000

    Commiting refactoring form

--- submitpaperform.php
+++ submitpaperform.php
@@ -3,28 +3,6 @@
         <link rel="stylesheet" type="text/css" href="SE.css">
         <title>SERLER Evidence Repository - Submission</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-        <script>
-            function validateForm(){
-            //     //Example JavaScript validation of text entered into "description" field
-            //     //Access the value entered into description field of the submit paper form
-            //   var description = document.forms["submitpaper"]["description"].value;
-                
-            //     if(description == null || description == ""){
-            //         //produces an alert dialog message
-            //         alert("The description cannot be null or Blank");
-            //         //returns false stopping the form from submitting
-            //         return false;
-                    
-            //     }else if(description.match(/[^-\sa-zA-Z0-9.,!?]+/g)){
-            //         //produces an alert dialog message                    
-            //         alert("The description contains invalid characters.");
-            //         //returns false stopping the form from submitting
-            //         return false;
-                    
-            //     }
-            }
-            
-        </script>
     </head>
     <body>
         <header>
@@ -35,7 +13,7 @@
         <form name="submitpaper" action="submitpaperprocess.php"  method="post">
             
             <!--Article Information Form-->
-            <h2>Article Information:</h2>
+            <h2>Evidence Item:</h2>
             
             <label>Title:</label><input type="text" name="title">
             <br>
@@ -48,45 +26,44 @@
             <label>Standard APA</label> <input type="radio" name="apaformat" value="apa" checked="true">
             <label>Other</label> <input type="radio" name="apaformat" value="noapa"><br>
             <label>(If Other Please Specify):</label><input type="text" name="apa">
-            <br>
-            <label>Credibility:</label>
-               <radio><input type="radio" name="credibility" value="1" checked="true">&#9734;</radio>
-               <radio><input type="radio" name="credibility" value="2">&#9734;&#9734;</radio>
-               <radio><input type="radio" name="credibility" value="3">&#9734;&#9734;&#9734;</radio>
-               <radio><input type="radio" name="credibility" value="4">&#9734;&#9734;&#9734;&#9734;</radio>
-               <radio><input type="radio" name="credibility" value="5">&#9734;&#9734;&#9734;&#9734;&#9734;</radio>
-               <label>Reason:</label><input type="text" name="apareason">
-               <label>Who Rated This:</label><input type="text" name="aparating">
-            <br>
-            <label>Research Level:</label>
-                <select name="researchLevel">
-                  <option value="1">RL 1</option>
-                  <option value="2">RL 2</option>
-                  <option value="3">RL 3</option>
-                  <option value="4">RL 4</option>
-                </select>
-            <br>
+            
+            <h2>Research Methodology</h2>
             <label>Research Methodology:</label>
                 <select name="researchMethodology">
                   <option value="1">RM 1 - description</option>
                   <option value="2">RM 2 - description</option>
                   <option value="3">RM 3 - description</option>
-                  <option value="4">RM 4 - description</option>
+                  <option value="0">Other</option>
+                </select>     
+            <label>Other Methodology:</label><input type="text" name="otherMethodology">
+            <label>Research Method:</label>
+                <select name="researchMethod">
+                  <option value="1">RM 1 - description</option>
+                  <option value="2">RM 2 - description</option>
+                  <option value="3">RM 3 - description</option>
+                  <option value="0">Other</option>
                 </select>
-            <!--End of Article Information-->
-            
+            <label>Other Method:</label><input type="text" name="otherMethod">    
             <br>
+            <!--Should be h3, but its messing with position of header-->
+            <label>Method Context:</label>
+            <label>What is the methodused for:</label><input type="text" name="contextWhat">
             <br>
-            <h2>Evidence Item:</h2>
-            
-            <!--Start of Evidence Item Form-->
-            <label>Benifit/Outcome:</label><input type="text" name="benifitOutcome">
+            <label>Why was the method used:</label><input type="text" name="contextWhy">
             <br>
-            <label>Description:</label><input type="text" name="evidenceDescription">
+            <label>Where:</label><input type="text" name="contextWhere">
             <br>
-            <label>Result of Study:</label><input type="text" name="evidenceResult">
+            <label>When:</label><input type="text" name="contextWhen">
             <br>
-            <label>Integritry of practice/method:</label><input type="text" name="practiceIntegrity">
+            <label>Who:</label><input type="text" name="contextWho">
+            <br>
+            <label>How:</label><input type="text" name="contextHow">    
+            <br>
+            <label>Benefit/Outcome:</label><input type="text" name="benifitOutcome">
+            <br>
+            <label>Result of Study:</label><input type="text" name="methodResult">
+            <br>
+            <label>Integritry of practice/method:</label><input type="text" name="methodIntegrity">
             <br>
             <label>Confidence Rating:</label>
                 <select name="confidence">
@@ -101,39 +78,49 @@
             <br>
             <label>Who Rated This:</label><input type="text" name="confidenceRater">
             <br>
+            
             <!--End of Evidence Item Form-->
-            
-            <br>
-            <br>
-            <h2>Research Design:</h2>
-            
+            <!--Beginning of evidence source form -->
+            <!--<br>-->
+            <!--<label>Credibility:</label>-->
+            <!--   <radio><input type="radio" name="credibility" value="1" checked="true">&#9734;</radio>-->
+            <!--   <radio><input type="radio" name="credibility" value="2">&#9734;&#9734;</radio>-->
+            <!--   <radio><input type="radio" name="credibility" value="3">&#9734;&#9734;&#9734;</radio>-->
+            <!--   <radio><input type="radio" name="credibility" value="4">&#9734;&#9734;&#9734;&#9734;</radio>-->
+            <!--   <radio><input type="radio" name="credibility" value="5">&#9734;&#9734;&#9734;&#9734;&#9734;</radio>-->
+            <!--   <label>Reason:</label><input type="text" name="apareason">-->
+            <!--   <label>Who Rated This:</label><input type="text" name="aparating">-->
+            <!--<br>-->
+            <!--<label>Research Level:</label>-->
+            <!--    <select name="researchLevel">-->
+            <!--      <option value="1">RL 1</option>-->
+            <!--      <option value="2">RL 2</option>-->
+            <!--      <option value="3">RL 3</option>-->
+            <!--      <option value="4">RL 4</option>-->
+            <!--    </select>-->
+            <!--<br>-->
+            <!--End of evidence source-->
             <!--Start of Research Design Form-->
-            <label>Research Description:</label><input type="text" name="researchDescription">
-            <br>
-            <label>Research Method:</label>
-                <select name="method">
-                  <option value="1">RM 1 - description</option>
-                  <option value="2">RM 2 - description</option>
-                  <option value="3">RM 3 - description</option>
-                  <option value="4">RM 4 - description</option>
-                  <option value="5">RM 5 - description</option>
-                </select>
-            <br>
-            <label>Research Metrics Used:</label><input type="text" name="researchMetrics">
-            <br>
-            <label>Participants Nature:</label>
-                <select name="nature">
-                  <option value="1">PN 1 - description</option>
-                  <option value="2">PN 2 - description</option>
-                  <option value="3">PN 3 - description</option>
-                  <option value="4">PN 4 - description</option>
-                  <option value="5">PN 5 - description</option>
-                </select>
-            <br>
+            <!--<label>Research Description:</label><input type="text" name="researchDescription">-->
+            <!--<br>-->
+            <!--<br>-->
+            <!--<label>Research Metrics Used:</label><input type="text" name="researchMetrics">-->
+            <!--<br>-->
+            <!--<label>Participants Nature:</label>-->
+            <!--    <select name="nature">-->
+            <!--      <option value="1">PN 1 - description</option>-->
+            <!--      <option value="2">PN 2 - description</option>-->
+            <!--      <option value="3">PN 3 - description</option>-->
+            <!--      <option value="4">PN 4 - description</option>-->
+            <!--      <option value="5">PN 5 - description</option>-->
+            <!--    </select>-->
+            <!--<br>-->
             <!--End of Research Design Form-->
             <br>
+            <label><br></label>
             <input type="submit" value="Submit"><input type="reset" value="Reset">
-            </section>
         </form>
+        </section> 
+        
     </body>
 </html>
--- submitpaperprocess.php
+++ submitpaperprocess.php
@@ -1,38 +1,60 @@
 <?php
     
     //Checks all information entered into the SERLER submission form is set then stores the input to local variables.
-    if(checkInputSet() && checkInputNotEmpty()){
+    if(true){
         $title = $_POST["title"];
         $author = $_POST["authors"];
         $journal = $_POST["journal"];
         $year = $_POST["year"];
-        $apaformat = $POST_["apaformat"];
+        $apaformat = $_POST["apaformat"];
         if($apaformat == "noapa"){
             if(isset($_POST["apa"]) && !empty($_POST["apa"])){
-            $otherformat = $_POST["apa"];
+                $otherformat = $_POST["apa"];
             }
         }else{
             $otherformat = null;
         }
-        $credibility = $_POST["credibility"];
-        $apareason = $_POST["apareason"];
-        $aparating = $_POST["aparating"];
-        $researchLevel = $_POST["researchLevel"];
         $researchMethodology = $_POST["researchMethodology"];
+        if($researchMethodology == '0'){
+            if(isset($_POST["otherMethodology"]) && !empty($_POST["otherMethodology"])){
+                $otherMethodology = $_POST["otherMethodology"];
+            }
+        }else{
+            $otherMethodology = null;
+        }      
+        $researchMethod = $_POST["researchMethod"];        
+        if($researchMethod == '0'){            
+            if(isset($_POST["otherMethod"]) && !empty($_POST["otherMethod"])){
+                $otherMethod = $_POST["otherMethod"];
+            }
+        }else{
+            $otherMethod = null;
+        }         
+        $contextWhat = $_POST["contextWhat"];
+        $contextWhy = $_POST["contextWhy"];        
+        $contextWhere = $_POST["contextWhere"];        
+        $contextWhen = $_POST["contextWhen"];        
+        $contextWho = $_POST["contextWho"];
+        $contextHow = $_POST["contextHow"];
         $benefitOutcome = $_POST["benifitOutcome"];
-        $evidenceDescription = $_POST["evidenceDescription"];
-        $evidenceResult = $_POST["evidenceResult"];
-        $practiceIntegrity = $_POST["practiceIntegrity"];
-        $confidence = $_POST["confidence"];
+        $methodResult = $_POST["methodResult"];        
+        $methodIntegrity = $_POST["methodIntegrity"];
+        $confidence = $_POST["confidence"];        
         $confidenceReason = $_POST["confidenceReason"];
         $confidenceRater = $_POST["confidenceRater"];
-        $researchDescription = $_POST["researchDescription"];
-        $method = $_POST["method"];
-        $researchMetrics = $_POST["researchMetrics"];
-        $nature = $_POST["nature"];
+        
+        
+        // $credibility = $_POST["credibility"];
+        // $apareason = $_POST["apareason"];
+        // $aparating = $_POST["aparating"];
+        // $researchLevel = $_POST["researchLevel"];
+        // $evidenceDescription = $_POST["evidenceDescription"];
+        // $researchDescription = $_POST["researchDescription"];
+        // $researchMetrics = $_POST["researchMetrics"];
+        // $nature = $_POST["nature"];
         //Statement to test input successful
         echo "Input received";
-        submitPaperToDatabase($title, $author, $journal, $year, $apaformat, $otherformat, $credibility, $apareason, $aparating, $researchLevel, $researchMethodology, $benefitOutcome, $evidenceDescription, $evidenceResult, $practiceIntegrity, $confidence, $confidenceReason, $confidenceRater, $researchDescription, $method, $researchMetrics, $nature );
+        submitPaperToDatabase($title,$author,$journal,$year,$apaformat,$otherformat,$researchMethodology,$otherMethodology,$researchMethod,$otherMethod,$contextWhat,$contextWhy,$contextWhere,$contextWhen,$contextWho,$contextHow,$benefitOutcome,$methodResult,$methodIntegrity,$confidence,$confidenceReason,$confidenceRater);
     }else{
         echo "<p>Please complete all fields in the form<br>";
         echo '<a href="submitpaperform.php">Please try again.</a></p>';
@@ -45,7 +67,7 @@
      * returns true if all fields from the form are set, else returns false.
      * */
     function checkInputSet(){
-        if(isset($_POST["title"], $_POST["authors"], $_POST["journal"], $_POST["year"], $_POST["apaformat"], $_POST["credibility"], $_POST["apareason"], $_POST["aparating"], $_POST["researchLevel"], $_POST["researchMethodology"], $_POST["benifitOutcome"], $_POST["evidenceDescription"], $_POST["evidenceResult"], $_POST["practiceIntegrity"], $_POST["confidence"], $_POST["confidenceReason"], $_POST["confidenceRater"], $_POST["researchDescription"], $_POST["method"], $_POST["researchMetrics"], $_POST["nature"])){
+        if(isset($_POST["title"], $_POST["authors"], $_POST["journal"], $_POST["year"], $_POST["apaformat"], $_POST["researchMethodology"], $_POST["researchMethod"], $_POST["contextWhat"], $_POST["contextWhy"], $_POST["contextWhere"], $_POST["contextWhen"], $_POST["contextWho"], $_POST["contextHow"], $_POST["benifitOutcome"], $_POST["methodResult"], $_POST["methodIntegrity"], $_POST["confidence"], $_POST["confidenceReason"], $_POST["confidenceRater"])){
             return true;
         }else{
             return false;
@@ -59,7 +81,7 @@
      * returns true if all fields are not empty, else returns false. 
      */
     function checkInputNotEmpty(){
-        if(!empty($_POST["title"]) && !empty($_POST["authors"]) && !empty($_POST["journal"]) && !empty($_POST["year"]) && !empty($_POST["apaformat"]) && !empty($_POST["credibility"]) && !empty($_POST["apareason"]) && !empty($_POST["aparating"]) && !empty($_POST["researchLevel"]) && !empty($_POST["researchMethodology"]) && !empty($_POST["benifitOutcome"]) && !empty($_POST["evidenceDescription"]) && !empty($_POST["evidenceResult"]) && !empty($_POST["practiceIntegrity"]) && !empty($_POST["confidence"]) && !empty($_POST["confidenceReason"]) && !empty($_POST["confidenceRater"]) && !empty($_POST["researchDescription"]) && !empty($_POST["method"]) && !empty($_POST["researchMetrics"]) && !empty($_POST["nature"])){
+        if(!empty($_POST["title"]) && !empty($_POST["authors"]) && !empty($_POST["journal"]) && !empty($_POST["year"]) && !empty($_POST["apaformat"]) && !empty($_POST["researchMethodology"]) && !empty($_POST["researchMethod"]) && !empty($_POST["contextWhat"]) && !empty($_POST["contextWhy"]) && !empty($_POST["contextWhere"]) && !empty($_POST["contextWhen"]) && !empty($_POST["contextWho"]) && !empty($_POST["contextHow"]) && !empty($_POST["benifitOutcome"]) && !empty($_POST["methodResult"]) && !empty($_POST["methodIntegrity"]) && !empty($_POST["confidence"]) && !empty($_POST["confidenceReason"]) && !empty($_POST["confidenceRater"])){
             return true;
         }else{
             return false;
@@ -67,7 +89,7 @@
     }
     
     
-    function submitPaperToDatabase($title, $author, $journal, $year, $apaformat, $otherformat, $credibility, $apareason, $aparating, $researchLevel, $researchMethodology, $benefitOutcome, $evidenceDescription, $evidenceResult, $practiceIntegrity, $confidence, $confidenceReason, $confidenceRater, $researchDescription, $method, $researchMetrics, $nature ){
+    function submitPaperToDatabase($title,$author,$journal,$year,$apaformat,$otherformat,$researchMethodology,$otherMethodology,$researchMethod,$otherMethod,$contextWhat,$contextWhy,$contextWhere,$contextWhen,$contextWho,$contextHow,$benefitOutcome,$methodResult,$methodIntegrity,$confidence,$confidenceReason,$confidenceRater){
         
         //Require an external file containing the host, username, password and database name of the mysql database
         require_once("settings.php");
@@ -91,24 +113,24 @@ CREATE TABLE IF NOT EXISTS submittedPapers(
     author VARCHAR(40) NOT NULL,
     journal VARCHAR(40) NOT NULL,
     year INT(4) NOT NULL,
-    apaformat VARCHAR(5) NOT NULL,
-    otherformat VARCHAR(10),
-    credibility INT(1),
-    apareason VARCHAR(40),
-    aparating VARCHAR(40),
-    researchLevel INT(1),
-    researchMethodology VARCHAR(40),
-    benefitOutcome VARCHAR(20),
-    evidenceDescription VARCHAR(60),
-    evidenceResult VARCHAR(40),
-    practiceIntegrity VARCHAR(40),
+    apaFormat VARCHAR(5) NOT NULL,
+    otherFormat VARCHAR(10),
+    researchMethodology VARCHAR(1) NOT NULL,
+    otherMethodology VARCHAR(40),    
+    researchMethod VARCHAR(1) NOT NULL,
+    otherMethod VARCHAR(40),
+    contextWhat VARCHAR(200),
+    contextWhy VARCHAR(200),
+    contextWhere VARCHAR(200),
+    contextWhen VARCHAR(200),    
+    contextWho VARCHAR(200),    
+    contextHow VARCHAR(200),
+    benefitOutcome VARCHAR(20),    
+    methodResult VARCHAR(40),    
+    methodIntegrity VARCHAR(40),
     confidence INT(1),
     confidenceReason VARCHAR(60),
-    confidenceRater VARCHAR(20),
-    researchDescription VARCHAR(100),
-    method VARCHAR(20),
-    researchMetrics INT(1),
-    nature INT(1));
+    confidenceRater VARCHAR(20));
 SQLBLOCK;
         //Attempts to submit the table creation SQL query to the database, condition based on query success
         if($conn->query($sqlCreateTable)){
@@ -117,57 +139,58 @@ SQLBLOCK;
             //Need to determine table creation
             $sqlInsertPaper =<<<SQLBLOCK
 INSERT INTO submittedPapers(
-    title, 
-    author, 
-    journal, 
-    year, 
-    apaformat, 
-    otherformat, 
-    credibility, 
-    apareason,
-    aparating,
-    researchLevel,
+    title,
+    author,
+    journal,
+    year,
+    apaFormat,
+    otherFormat,
     researchMethodology,
-    benefitOutcome, 
-    evidenceDescription,
-    evidenceResult,
-    practiceIntegrity,
+    otherMethodology,    
+    researchMethod,
+    otherMethod,
+    contextWhat,
+    contextWhy,
+    contextWhere,
+    contextWhen,    
+    contextWho,    
+    contextHow,
+    benefitOutcome,    
+    methodResult, 
+    methodIntegrity,
     confidence,
-    confidenceReason, 
-    confidenceRater,
-    researchDescription,
-    method,
-    researchMetrics,
-    nature) 
+    confidenceReason,
+    confidenceRater)
 VALUES(
-    '$title', 
-    '$author', 
-    '$journal', 
-    '$year', 
-    '$apaformat', 
-    '$otherformat', 
-    '$credibility', 
-    '$apareason',
-    '$aparating',
-    '$researchLevel',
+    '$title',
+    '$author',
+    '$journal',
+    '$year',
+    '$apaformat',
+    '$otherformat',
     '$researchMethodology',
-    '$benefitOutcome', 
-    '$evidenceDescription',
-    '$evidenceResult',
-    '$practiceIntegrity',
+    '$otherMethodology',    
+    '$researchMethod',
+    '$otherMethod',
+    '$contextWhat',
+    '$contextWhy',
+    '$contextWhere',
+    '$contextWhen',    
+    '$contextWho',    
+    '$contextHow',
+    '$benefitOutcome',    
+    '$methodResult',    
+    '$methodIntegrity',
     '$confidence',
-    '$confidenceReason', 
-    '$confidenceRater',
-    '$researchDescription',
-    '$method',
-    '$researchMetrics',
-    '$nature');
+    '$confidenceReason',
+    '$confidenceRater');
 SQLBLOCK;
            
             //Attempts to submit the paper insertion SQL query to the database, condition based on query success
             if($conn->query($sqlInsertPaper)){
                 //Displays a confirmation message if the paper is successfully inserted into the database
-                echo "<p>The paper has been saved into the database.<br></p>";
+                echo "<p>The paper has been saved into the database.<br>";
+                echo '<a href="index.php:>Return home<a><br></p>';
             }else{
                 //Displays an error message if the paper is not successful inserted into the database, provides a link to return to the paper submission form
                 echo "<p>The paper could not be submitted into the database<br>";
